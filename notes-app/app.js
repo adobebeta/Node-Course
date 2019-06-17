d677ff1d95c440abe3c8e97b602c80d029db4ba6
adobebeta <adobebeta1997@gmail.com>
2019-06-17 15:52:59 +0700

    sun func

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,5 @@
-const name = require('./utils.js')
-console.log(name)
+const add = require('./utils.js')
+
+const sum = add(4,-2)
+
+console.log(sum)
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,5 +1,8 @@
 console.log('utils.js')
 
 const name = 'Zoom'
+const add = function(a,b){
+    return a+b
+}
 
-module.exports = name
+module.exports = add
