bc88d04047685541a3b8e17e3b74b659d110d046
adobebeta <adobebeta1997@gmail.com>
2019-06-17 15:32:56 +0700

    apped to file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,8 @@
 const fs = require('fs')
 
 fs.writeFileSync('notes.txt' , 'My name is zoom!')
+
+fs.appendFile('notes.txt', 'data to append', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
