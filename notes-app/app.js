256311eae7554e13c1da67ec3be8c18921dfd1d8
adobebeta <adobebeta1997@gmail.com>
2019-06-17 16:17:07 +0700

    npm i validator //check isEmail validator function

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,12 @@
- const getNotes = require('./notes')
+const validator = require('validator')
+const getNotes = require('./notes')
+
+
 
  const msg = getNotes()
 
  console.log(msg)
+
+ console.log(validator.isEmail('ss'))
+ 
+ console.log(validator.isEmail('ss@gmail.com'))
