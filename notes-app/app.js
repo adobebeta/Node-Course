4365aa02c6dd0c103060c0a1777f25ebef1d5913
adobebeta <adobebeta1997@gmail.com>
2019-06-19 10:47:59 +0700

    yarg and json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,22 @@
 const chalk = require('chalk');
 const getNotes = require('./notes')
-
-
+const yargs = require('yargs')
 const command = process.argv[2]
 
-if (command === 'add'){
-    console.log('Adding note!')
-} else if (command === 'remove'){
-    console.log('Removing Note!')
-}
+
+// customize yargs version
+yargs.version('1.1.0')
+
+//crate add command
+yargs.command({
+    command: 'add' ,
+    describe : 'Add a new note',
+    handler : function(){
+        console.log('Adding a new note!')
+    }
+})
+
+//add ,remove , read , list
+
+
+console.log(yargs.argv)
---
+++ playground/1-json.js
@@ -0,0 +1,7 @@
+const book = {
+    title: 'Egg is the Enemy',
+    author : 'Ryan Holiday'
+}
+
+const bookJSON = JSON.stringify(book)
+console.log(bookJSON)
