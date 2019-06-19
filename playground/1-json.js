ea547f62b15a38f22e1a98f5c5b466a8f222c62a
adobebeta <adobebeta1997@gmail.com>
2019-06-19 10:56:56 +0700

    writeFile JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 const book = {
     title: 'Egg is the Enemy',
     author : 'Ryan Holiday'
 }
 
 const bookJSON = JSON.stringify(book)
-console.log(bookJSON)
+fs.writeFileSync('1-json.json', bookJSON)
